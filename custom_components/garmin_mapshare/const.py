4a9b028f51dff18e352de9aa8f5ab5958630208e
Stephen Eisenhauer <bhs2007@gmail.com>
2024-01-06 10:56:55 -0800

    Use region-agnostic base URL for feeds (issues #2 and #4)

--- custom_components/garmin_mapshare/const.py
+++ custom_components/garmin_mapshare/const.py
@@ -5,7 +5,8 @@ DOMAIN = "garmin_mapshare"
 CONF_LINK_NAME = "link_name"
 CONF_LINK_PASSWORD = "link_password"
 
-BASE_URL = "https://explore.garmin.com/Feed/Share/"
+# Using share.garmin.com seems to work in all regions; explore.garmin.com only worked for US accounts?
+BASE_URL = "https://share.garmin.com/Feed/Share/"
 WEB_BASE_URL = "https://share.garmin.com/share/"
 
 MANUFACTURER = "Garmin"
